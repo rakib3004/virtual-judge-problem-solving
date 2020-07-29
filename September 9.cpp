1653ca13e8ade20722ed9577741421c9723b3705
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-29 19:57:40 +0600

    Create September 9.cpp

---
+++ September 9.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int n;
+
+cin>>n;
+
+int a,b;
+
+a= n/10;
+b=n%10;
+
+if(a==9||b==9){
+cout<<"Yes"<<endl;
+}else{
+cout<<"No"<<endl;
+
+}
+return 0;
+}
