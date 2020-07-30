a19f2ff2ca0358712c15202eb05c4ac3589814ab
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-07-30 21:25:20 +0600

    Create Infinity Coins.cpp

---
+++ Infinity Coins.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+
+int a;
+
+cin>>n;
+cin>>a;
+n = n%500;
+if(a>=n){
+cout<<"Yes"<<endl;
+}else{
+cout<<"No"<<endl;
+
+}
+return 0;
+}
